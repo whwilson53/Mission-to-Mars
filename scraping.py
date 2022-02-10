761f926699f516053d70c68f07f749400254f3d7
abc <will.h.wilson@gmail.com>
2022-02-10 13:24:50 -0500

    commit at 10.5.3

--- scraping.py
+++ scraping.py
@@ -41,7 +41,6 @@ def mars_news(browser):
     #Convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = soup(html, 'html.parser')
-    slide_elem = news_soup.select_one('div.list_text')
     
     #Add try/except for error handling
     try:
@@ -54,7 +53,7 @@ def mars_news(browser):
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
     
     except AttributeError:
-        return None,None
+        return None, None
     
     return news_title, news_p
 
